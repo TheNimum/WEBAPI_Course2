859b69f0606cbe2d1d90e3812242b759c892e2ae
TheNimum <mikael.pajari@live.se>
2021-02-26 19:07:03 +0100

    uppdate

--- WEBAPI_Course2/Controllers/ProductsController.cs
+++ WEBAPI_Course2/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using WEBAPI_Course2.Models;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 
 namespace WEBAPI_Course2.Controllers
 {
@@ -34,6 +35,30 @@ namespace WEBAPI_Course2.Controllers
                 return new HttpStatusCodeResult(204);
         
             return new JsonResult { Data = selectedbook, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+        [HttpGet]
+        public ActionResult GetAllBooksInfo()
+        {
+            List<Books> BInfo = new List<Books>
+            {
+                new Books() { ID = 1001, BookAuthor = "jesus", Titel = "Bibel", BookInfo = "Tråkig bok" },
+                new Books() { ID = 1002, BookAuthor = "James white", Titel = "Fotball", BookInfo = "bra book" },
+                new Books() { ID = 1003, BookAuthor = "Mark J Price ", Titel = "c# något", BookInfo = "skolbok" },
+                new Books() { ID = 1004, BookAuthor = "Bonkers ", Titel = "barnboken", BookInfo = "lalal" }
+            };
+            return new JsonResult { Data = BInfo, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+        
+        [HttpPost]
+        public ActionResult AddBook(Books book)
+        {
+            List<Books> add_book = new List<Books>();
+            add_book.Add(book);
+
+
+
+            return new HttpStatusCodeResult(200);
         }
     }
 }
